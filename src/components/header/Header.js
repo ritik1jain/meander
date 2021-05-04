e0683c8a651eb6623b55b6c54aabf7d0e7953834
ritik1jain <ritikjain0706.com>
2021-05-04 18:55:25 +0530

    day2:adding video feed

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+<div className="row">
+  <div className="stream-box-width">
+    <span className="btn float-start live-btn">Go Live</span>
+    <span className="float-end live-studio">Live Studio</span>
+  </div>
+</div>
+
+    )
+}
+
+export default Header
